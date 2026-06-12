123662c96f8192ad8904d6f885137029fe36da76
vitorfehelberg <vitor.fehelberg@gmail.com>
2026-06-12 20:01:28 +0000

    feat(dashboard): add cohort dimension parameter
    - Add cohort dimension parameter to dashboard.

---
+++ queries/parameters/param_cohort_dimension.sql
@@ -0,0 +1,11 @@
+SELECT 'account_status' AS cohort_dimension
+ UNION ALL
+SELECT 'vocation'
+ UNION ALL
+SELECT 'world'
+ UNION ALL
+SELECT 'in_guild'
+ UNION ALL
+SELECT 'owns_house'
+ UNION ALL
+SELECT 'is_married'
